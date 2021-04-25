b45df072a1e123dad894c3a0c3f7013d9cfa84dd
tamaron <49875900+tamaron@users.noreply.github.com>
2021-04-26 02:10:46 +0900

    Text入力エリアを追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,4 +6,13 @@ using UniRx;
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
     public Subject<Unit> Setup = new Subject<Unit>();
+    [SerializeField] GameObject textPanel;
+    protected override void Awake()
+    {
+        base.Awake();
+        Setup.Subscribe(_ =>
+        {
+            textPanel.SetActive(true);
+        }).AddTo(this);
+    }
 }
--- Assets/Scripts/WindowManage.cs
+++ Assets/Scripts/WindowManage.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class WindowManage : SingletonMonoBehaviour<MonoBehaviour>
+public class WindowManage : SingletonMonoBehaviour<WindowManage>
 {
-    
+    static public int _width = 1024, _height = 768;
+
 }
