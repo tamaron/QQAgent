3d0c87d2dfa29384da8cd535bec3e927368d5d86
tamaron <49875900+tamaron@users.noreply.github.com>
2021-04-30 02:36:24 +0900

    LitJsonの追加　「天気」というキーワードで現在の天気を返すようにした

--- Assets/Scripts/Model/TextGroupModel.cs
+++ Assets/Scripts/Model/TextGroupModel.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.Networking;
 using DG.Tweening;
-using System.Threading.Tasks;
 using UniRx;
 using Cysharp.Threading.Tasks;
+using LitJson;
 
 public class TextGroupModel : MonoBehaviour
 {
@@ -16,19 +18,40 @@ public class TextGroupModel : MonoBehaviour
 
     public async UniTask<string> GetResultAsync(string text)
     {
-        string url = text;
         string result;
-        UnityWebRequest uwr = UnityWebRequest.Get(url);
-        try
+        switch (GetMessageType(text))
         {
-            await uwr.SendWebRequest();
-            result = uwr.downloadHandler.text;
-        }
-        catch
-        {
-            result = errorMessage;
+            case MessageType.Weather:
+                string url = "https://weather.tsukumijima.net/api/forecast?city=040010";
+                try
+                {
+                    UnityWebRequest uwr = UnityWebRequest.Get(url);
+                    await uwr.SendWebRequest();
+                    JsonData jsonData = JsonMapper.ToObject(uwr.downloadHandler.text);
+                    result = $"{(string)jsonData["title"]}\r\n{(string)jsonData["forecasts"][0]["telop"]}";
+                }
+                catch
+                {
+                    result = errorMessage;
+                }
+                break;
+            default:
+                result = errorMessage;
+                break;
+
         }
         return result;
     }
 
+    public MessageType GetMessageType(string text)
+    {
+        if (Regex.IsMatch(text, "天気")) return MessageType.Weather;
+        return MessageType.None;
+    }
+}
+
+public enum MessageType
+{
+    None,
+    Weather
 }
