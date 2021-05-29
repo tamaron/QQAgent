988440035737b40f028b04f4273bd56902827d68
tamaron <49875900+tamaron@users.noreply.github.com>
2021-05-29 23:58:29 +0900

    Revert "Delete Credential.cs"
    This reverts commit 742a2b092799144bb1d9239cb5ed46f7e2d9bf69.

---
+++ Assets/Scripts/Credentials/Credential.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// API‚È‚Ç‚Ìî•ñ‚Ì•Û
+/// </summary>
+public static class Credential
+{
+    public const string OPEN_WEATHER_MAP_API_KEY = "aaa";
+    public const string CLOUD_SPEECHTOTEXT_API_KEY = "aaa";
+    public const string MECAB_API_URLBASE = "aaa";
+}
