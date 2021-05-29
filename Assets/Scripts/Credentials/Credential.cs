39957f205812f9761343cdd4c930876bcba5cbb9
tamaron <49875900+tamaron@users.noreply.github.com>
2021-05-30 00:04:42 +0900

    キャッシュを削除

--- Assets/Scripts/Credentials/Credential.cs
+++
@@ -1,9 +0,0 @@
-/// <summary>
-/// API‚È‚Ç‚Ìî•ñ‚Ì•Û
-/// </summary>
-public static class Credential
-{
-    public const string OPEN_WEATHER_MAP_API_KEY = "aaa";
-    public const string CLOUD_SPEECHTOTEXT_API_KEY = "aaa";
-    public const string MECAB_API_URLBASE = "aaa";
-}
