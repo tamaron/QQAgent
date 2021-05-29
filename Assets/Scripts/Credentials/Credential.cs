c2d42d750d44052938fba51273501195ea96fb72
tamaron <49875900+tamaron@users.noreply.github.com>
2021-05-29 23:40:44 +0900

    Update Credential.cs

--- Assets/Scripts/Credentials/Credential.cs
+++ Assets/Scripts/Credentials/Credential.cs
@@ -3,7 +3,7 @@
 /// </summary>
 public static class Credential
 {
-    public const string OPEN_WEATHER_MAP_API_KEY = "test";
-    public const string CLOUD_SPEECHTOTEXT_API_KEY = "test";
-    public const string MECAB_API_URLBASE = "test";
+    public const string OPEN_WEATHER_MAP_API_KEY = "aaa";
+    public const string CLOUD_SPEECHTOTEXT_API_KEY = "aaa";
+    public const string MECAB_API_URLBASE = "aaa";
 }
